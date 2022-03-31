99cd9e260ffe73d3ef256d74bf5cc1951f8ffcd5
bpichery <pichery.benedicte@gmail.com>
2022-03-31 18:40:00 +0200

    adaptation for heroku

--- nuxt.config.js
+++ nuxt.config.js
@@ -67,15 +67,8 @@ export default {
     }
   },
   axios: {
-    proxy: true,
+    baseURL: 'https://courses-chantier-backend.herokuapp.com/', // Used as fallback if no runtime config is provided
   },
-  proxy: {
-      '/api': {
-        target: 'http://backend-courseschantier.herokuapp.com/',
-        changeOrigin: true,
-        pathRewrite: { '^/api': '/' },
-      }
-    },
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
 }
